156fd61b06daf0809f31c352826b40bf23a57c6c
Dan Chif <nadchif@gmail.com>
2021-10-25 12:19:39 +0800

    Update env.js

--- src/env.js
+++ src/env.js
@@ -2,5 +2,5 @@ export const ENV_CONSTANTS = {
   apiUrl:
     process.env.REACT_APP_HOST_TYPE !== "production"
       ? "http://localhost:8000/api"
-      : "http://salty-coast-16931.herokuapp.com/api",
+      : "https://salty-coast-16931.herokuapp.com/api",
 };
